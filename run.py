f972d8ced4d62003c29d9c45512b73853724b8ec
Lsverry <nmx0040@gmail.com>
2024-01-28 10:29:52 +0000

    Create player class with its attributes and instance methods.

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+class player():
+    """
+    Create an instance of the player class
+    """
+    def __init__(self, name):
+        self.name = name
+        self.tries = 0 #Attribute specific to each instance
+        self.correct_points = 0
+        self.wrong_points = 0 
+        self.wrong_words = []
+        self.correct_words = []
+
+
+    def add_points(self, name_counter):
+        if name_counter == "tries":
+            self.tries+=1
+        elif name_counter == "correct_points":
+            self.correct_points+=1
+        else:
+            self.wrong_points+=1
+
+    def add_word(self, name_list, word):
+        if name_list == "wrong_words":
+            self.wrong_words.append(word)
+        else:
+            self.correct_words.append(word)
